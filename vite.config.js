c6835ce0fd6841940f06cd4121b875dda4f08dd4
TNTSuperMan <tntsuperga@gmail.com>
2024-07-02 21:07:49 +0900

    publish

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,31 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteSingleFile()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  root: "src",
+  base: "./",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: "src/index.html",
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]"
+      }
     }
   }
 })
