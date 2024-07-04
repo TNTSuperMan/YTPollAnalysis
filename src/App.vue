8c5fef22372e894720172bf70665164e60f048e4
TNTSuperMan <tntsuperga@gmail.com>
2024-07-04 16:30:46 +0900

    credit

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,6 @@ import {ref,computed} from "vue"
 import Option from './components/Option.vue'
 const votecount = ref(0)
 const polldata = ref([])
-const state = computed(()=>{
-    if(polldata.value.length == 0){
-        return "選択肢がありません"
-    }
-    let per = 0
-    polldata.value.forEach(e=>per+=e.rate)
-    if(per != 100){
-        return "割合の合計が100%ではありません"
-    }
-    let sum = 0
-    polldata.value.forEach(e=>
-        sum += Math.round(votecount.value * (e.rate / 100))
-    )
-    if(sum == votecount.value){
-        return "結果は正しいです"
-    }else{
-        return "結果は誤差があります"
-    }
-})
 </script>
 <template>
     <div class="box">
@@ -31,8 +12,16 @@ const state = computed(()=>{
         :vc="votecount" @del="polldata.splice(i,1)"/>
         <br><button @click="polldata.push({name:'',rate:0})" class="add">選択肢をさらに追加</button>
     </div>
-    
-    <p>{{ state }}</p>
+    <details>
+        <summary>クレジット</summary>
+        <table>
+            <tr><td>コードエディタ</td><td><a target="_blank" href="https://code.visualstudio.com">Visual Studio Code</a></td></tr>
+            <tr><td>フレームワーク</td><td><a target="_blank" href="https://vuejs.org">Vue</a></td></tr>
+            <tr><td>コンパイラ</td><td><a target="_blank" href="https://vitejs.dev">Vite</a></td></tr>
+            <tr><td>パッケージ管理</td><td><a target="_blank" href="https://www.npmjs.com">npm</a></td></tr>
+            <tr><td>スタイル</td><td><a target="_blank" href="https://www.google.com">Google</a></td></tr>
+        </table>
+    </details>
 </template>
 <style scoped>
 .title{
@@ -44,5 +33,21 @@ const state = computed(()=>{
 .vcinp{
     color:#aaa;
     border:none;
+}
+td{
+    padding:3px;
+}
+td a{
+    color:#0ff;
+    text-decoration: none;
+    transition:all 0.3s ease;
+}
+td a:hover{
+    color:#00f;
+    text-decoration: underline;
+}
+summary{
+    cursor:pointer;
+    user-select: none;
 }
 </style>
